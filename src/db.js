e18c7d1f557c714dca314350eb43feecb34ca3bb
Stefan Gründel <stefan.gruendel@gmail.com>
2023-12-27 15:50:13 +0100

    update dependencies

--- src/db.js
+++ src/db.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/ecowitt');
 
 var exports = module.exports = {};
